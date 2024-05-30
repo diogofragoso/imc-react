462d8e347d9b716767c8fd9415df3e4e7dbed2b0
diogofragoso <diogofragoso@yahoo.com.br>
2024-05-30 18:55:55 -0300

    ajuste dos cards

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -6,14 +6,26 @@ function Content() {
 
 
 
-    function CalcularImc() {
 
-        alert("E ai broa de milho")
-
-
-    }
 
     const [resultado, setResultado] = useState();
+    const [peso, setPeso] = useState();
+    const [altura, setAltura] = useState();
+    const [botao, setBotao] = useState();
+    const [exibirResultado, setExibirResultado] = useState('Resultado');
+
+
+
+
+    function Resultado() {
+
+        let novoResultado = peso / (altura * altura);
+        setResultado(novoResultado.toFixed(2));
+
+
+        // alert(resultado);
+
+    }
 
 
 
@@ -33,41 +45,59 @@ function Content() {
                                         <Label for="example">
                                             Peso
                                         </Label>
-                                        <Input valid placeholder="Digite o peso" />
+                                        <Input
+                                            type="number"
+                                            id="pesoInput"
+                                            valid placeholder="Digite o peso"
+                                            onChange={e => setPeso(parseFloat(e.target.value))}
+                                            value={peso}
 
+                                        />
 
                                         <Label for="example">
                                             Altura
                                         </Label>
-                                        <Input valid placeholder="Digite a altura" />
+                                        <Input
+                                            id="alturaInput"
+                                            type="number"
+                                            valid placeholder="Digite a altura"
+                                            onChange={e => setAltura(parseFloat(e.target.value))}
+                                            value={altura}
+                                        />
 
 
                                     </FormGroup>
 
                                 </Form>
                             </CardText>
-                                    <Button
-                                        type="button"
-                                        onClick={CalcularImc}
-                                    >
-                                        Calcular
-                                    </Button>
+                            <Button
+                                type="button"
+                                onClick={Resultado}
+                            >
+                                Calcular
+                            </Button>
 
 
 
                         </Card>
                     </Col>
                     <Col sm="6">
-                        <Card body>
+                        <Card body className={styles.card}>
                             <CardTitle tag="h5">
-                                Special Title Treatment
+                                {resultado !== null ? (
+                                    <h2 id="exibirResultado">
+                                        Resultado: {resultado}
+                                    </h2>
+                                ) : (
+                                    <h2 id="exibirResultado">
+                                        Nenhum resultado
+                                    </h2>
+                                )}
+
                             </CardTitle>
-                            <CardText>
-                                With supporting text below as a natural lead-in to additional content.
-                            </CardText>
-                            <Button>
-                                Go somewhere
-                            </Button>
+
+
+
                         </Card>
                     </Col>
                 </Row>
